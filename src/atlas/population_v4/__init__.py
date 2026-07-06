6b825eb674c1d179b8d057a5910400cceb3da178
Michael Elvis Brockway <mbrockway11@yahoo.com>
2026-07-06 00:34:01 -0700

    Add Population Intelligence v4 feature index

--- src/atlas/population_v4/__init__.py
+++ src/atlas/population_v4/__init__.py
@@ -3,8 +3,11 @@
 
 from atlas.population_v4.corpus import build_population_v4_corpus
 from atlas.population_v4.features import build_population_feature_record
+from atlas.population_v4.feature_index import build_population_feature_index, query_feature_index
 
 __all__ = [
     "build_population_v4_corpus",
     "build_population_feature_record",
+    "build_population_feature_index",
+    "query_feature_index",
 ]
