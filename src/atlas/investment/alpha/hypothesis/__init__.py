e0c879fb228efc757db511dd9cf2c31e529aac82
Michael Elvis Brockway <mbrockway11@yahoo.com>
2026-07-07 19:09:07 -0700

    Add Alpha Hypothesis Generator

---
+++ src/atlas/investment/alpha/hypothesis/__init__.py
@@ -0,0 +1,10 @@
+
+"""Alpha Hypothesis Generator."""
+
+from atlas.investment.alpha.hypothesis.generator import build_alpha_hypotheses
+from atlas.investment.alpha.hypothesis.report import build_alpha_hypothesis_report
+
+__all__ = [
+    "build_alpha_hypotheses",
+    "build_alpha_hypothesis_report",
+]
