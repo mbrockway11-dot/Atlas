fae119a5878c1f9849072e930ed96e3fac34b6dd
Michael Elvis Brockway <mbrockway11@yahoo.com>
2026-07-13 18:47:48 -0700

    Add G.19 generic REST paper broker adapter

--- src/atlas/investment/brokers/__init__.py
+++ src/atlas/investment/brokers/__init__.py
@@ -25,6 +25,20 @@ from .errors import (
     BrokerNotFoundError,
     BrokerValidationError,
 )
+from .fixtures import FixtureRestTransport
+from .generic_rest_paper import (
+    GenericRestPaperAdapter,
+    GenericRestPaperConfig,
+    RestAuthenticationError,
+    RestRateLimitError,
+    RestServerError,
+    RestTransport,
+    RestTransportError,
+    normalize_order_status,
+    normalize_order_type,
+    normalize_side,
+    normalize_symbol,
+)
 from .mock import MockBrokerAdapter, PaperOnlyExecutionGate
 from .registry import (
     DEFAULT_BROKER_PROVIDER,
@@ -59,8 +73,20 @@ __all__ = [
     "BrokerValidationError",
     "DEFAULT_BROKER_PROVIDER",
     "ExecutionModeGate",
+    "FixtureRestTransport",
+    "GenericRestPaperAdapter",
+    "GenericRestPaperConfig",
     "MockBrokerAdapter",
     "PaperOnlyExecutionGate",
+    "RestAuthenticationError",
+    "RestRateLimitError",
+    "RestServerError",
+    "RestTransport",
+    "RestTransportError",
     "default_registry",
+    "normalize_order_status",
+    "normalize_order_type",
+    "normalize_side",
+    "normalize_symbol",
     "validate_order_capabilities",
 ]
